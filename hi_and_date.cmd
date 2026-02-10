515a9b2e103353876922802718aa021667d70e0c
efyme11 <efyme11@nottingham.ac.uk>
2026-02-10 14:07:07 +0000

    Add welcome/date batch script and update README

---
+++ hi_and_date.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo =====================================
+echo   Welcome! Have a great day 🙂
+echo =====================================
+echo.
+echo Current Date: %DATE%
+echo Current Time: %TIME%
+echo.
+pause
